315a7f706b5c61c340b887c034d68f9ced6a4ed5
TataCthullu <gonzalo.org@gmail.com>
2024-04-04 03:11:12 -0300

    cuarto com

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
@@ -34,26 +34,16 @@ inicializar_base_de_datos()
 # Ruta para la página de inicio
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/seleccionar_destinos')
+def seleccionar_destinos():
     conexion = sqlite3.connect('base_de_datos.db')
     cursor = conexion.cursor()
     cursor.execute("SELECT DISTINCT nombre FROM destinos")
     destinos = cursor.fetchall()
     conexion.close()
-    return render_template('index.html', destinos=destinos)
-
-# Ruta para seleccionar un destino
-@app.route('/seleccionar-destino', methods=['GET', 'POST'])
-def seleccionar_destino():
-    if request.method == 'POST':
-        destino_seleccionado = request.form['destino']
-        return redirect(url_for('info_destino', destino=destino_seleccionado))
-    else:
-        conexion = sqlite3.connect('base_de_datos.db')
-        cursor = conexion.cursor()
-        cursor.execute("SELECT nombre FROM destinos")
-        destinos = cursor.fetchall()
-        conexion.close()
-        return render_template('seleccionar_destino.html', destinos=destinos)
+    return render_template('seleccionar_destinos.html', destinos=destinos)
 
 # Ruta para mostrar información sobre el destino seleccionado
 @app.route('/info-destino/<destino>')
@@ -67,5 +57,3 @@ def info_destino(destino):
 
 if __name__ == '__main__':
     app.run(debug=True)
-
- #nadaqverman   
